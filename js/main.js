798463bd3fd717971862075a459af6639fb39d39
jkosturko <jessica.kosturko@gmail.com>
2016-01-22 18:41:06 -0500

    arrows, imports, webpack, other neat stuff

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
-var calculateMonthlyPayment = function (principal, years, rate) {
-    let monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
-    }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return {principal, years, rate, monthlyPayment, monthlyRate};
-};
+import * as mortgage from './mortgage';
 
-document.getElementById('calcBtn').addEventListener('click', function () {
+document.getElementById('calcBtn').addEventListener('click', () => {
 
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
 
-    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate, amorization} =
+     mortgage.calculateAmortization(principal, years, rate);
 
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+
+    amorization.forEach(month => console.log(month));
+
+    let html = "";
+    amorization.forEach((year, index) => html += `
+        <tr>
+            <td>${index + 1}</td>
+            <td class="currency">${Math.round(year.principalY)}</td>
+            <td class="stretch">
+                <div class="flex">
+                    <div class="bar principal"
+                         style="flex:${year.principalY};-webkit-flex:${year.principalY}">
+                    </div>
+                    <div class="bar interest"
+                         style="flex:${year.interestY};-webkit-flex:${year.interestY}">
+                    </div>
+                </div>
+            </td>
+            <td class="currency left">${Math.round(year.interestY)}</td>
+            <td class="currency">${Math.round(year.balance)}</td>
+        </tr>
+    `);
+
+document.getElementById("amortization").innerHTML = html;
 
 });
